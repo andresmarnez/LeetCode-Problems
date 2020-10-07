178c9a28caafac4e2aa5bbc237e91de9b70e2fe0
Andrés Sánchez <andresmarnez@gmail.com>
2020-10-07 23:38:07 +0200

    1st exercise

---
+++ question1.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+var zeroes  = [];
+    
+    for (var i = 0 ; i < matrix.length ; i++){
+        
+        for (var j = 0 ; j < matrix[i].length ; j++){
+            
+            if(matrix[i][j] === 0){
+                zeroes.push([i,j]);
+            };
+        };
+    };
+    
+    for(var h = 0; h < zeroes.length; h++){
+        eliminai(matrix,zeroes[h][0],zeroes[h][1]);
+            
+    };
+};
+
+
+var eliminai = (total, x, y) => {
+
+    for(var j = 0; j < total[x].length; j++ ){
+        total[x][j] = 0;
+    };
+    
+    for(var i = 0; i < total.length; i++ ){
+        total[i][y] = 0;
+    };
+};
+
